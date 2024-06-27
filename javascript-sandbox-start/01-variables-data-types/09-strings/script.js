fd2aa5162400e098c7d6e134ddc007e64452f727
Runqing-Programer <maorunqing1997@gmail.com>
2024-06-27 19:49:35 +1000

    update to  1.9

---
+++ javascript-sandbox-start/01-variables-data-types/09-strings/script.js
@@ -0,0 +1,23 @@
+const name = 'John';
+const age = 30;
+
+console.log(`Hello, my name is ${name} and I am ${age} years old`);
+
+const s = 'new string';
+
+s.toUpperCase();
+
+console.log(s);
+
+let x;
+x = s.substring(5);
+
+x = s.slice(-1, -4);
+
+x = s.replace('new', 'brand new');
+
+x = s.trim();
+
+x = s.split('');
+
+console.log(x);
