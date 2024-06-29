aacc1b9d7d05bae0f00ae04769338c95b96c8e6f
Runqing-Programer <maorunqing1997@gmail.com>
2024-06-29 12:28:14 +1000

    update to math

--- javascript-sandbox-start/01-variables-data-types/12-math-object/script.js
+++ javascript-sandbox-start/01-variables-data-types/12-math-object/script.js
@@ -1,1 +1,19 @@
 console.log(Math)
+
+const num = new  Number(5)
+
+console.log(num,typeof num)
+
+let x;
+
+x = num.toFixed(2) 
+
+x = num.toPrecision(3)
+
+x = num.toLocaleString('zh-CN')
+
+x = Number.MAX_VALUE.toString();
+
+
+
+console.log(x);
