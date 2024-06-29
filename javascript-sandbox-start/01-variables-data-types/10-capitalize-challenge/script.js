ad8e99cec76ed0149a053bead02d1af5bfe0e7bd
Runqing-Programer <maorunqing1997@gmail.com>
2024-06-29 12:21:50 +1000

    Sat  29 Jun

--- javascript-sandbox-start/01-variables-data-types/10-capitalize-challenge/script.js
+++ javascript-sandbox-start/01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,17 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
 const myString = 'developer';
+
+// My solution
+function capFirstLetter(str) {
+	const firstChar = String(str[0]);
+	const restOfString = str.split(str[0]);
+
+	return firstChar.toUpperCase() + restOfString[1];
+}
+
+console.log(capFirstLetter(myString));
+
+// Solutions 
+let myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
+
+console.log(myNewString);
---
+++ javascript-sandbox-start/01-variables-data-types/11-numbers/script.js
@@ -0,0 +1,16 @@
+
+const num = new  Number(5)
+
+console.log(num,typeof num)
+
+let x;
+
+x = num.toFixed(2) 
+
+x = num.toPrecision(3)
+
+x = num.toLocaleString('zh-CN')
+
+x = Number.MAX_VALUE.toString();
+
+console.log(x);
