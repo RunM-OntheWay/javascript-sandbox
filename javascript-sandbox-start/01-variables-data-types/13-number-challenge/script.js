e518df351016a6ae86f25cf0639d9d98ea02ef8c
Runqing-Programer <maorunqing1997@gmail.com>
2024-06-29 13:03:00 +1000

    num challenge

---
+++ javascript-sandbox-start/01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,17 @@
+const x = Math.floor(Math.random()*100+1)
+const y = Math.floor(Math.random()*50+1)
+
+const func1 = x+y;
+const output1  =`${x} + ${y} = ${func1}`
+
+console.log(output1)
+
+console.log(x)
+console.log(y)
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+
+
